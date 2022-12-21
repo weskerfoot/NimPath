bb71d0b8ed67f3aa7ac642f6402d0de2d116c373
Wesley Kerfoot <wjak56@gmail.com>
2022-12-21 15:39:35 -0500

    pass parameter for base url

--- nimpath.nimble
+++ nimpath.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.0"
+version       = "0.1.1"
 author        = "Wesley Kerfoot"
 description   = "Interface to libxml2's XPath parser"
 license       = "MIT"
--- src/nimpathpkg/parser.nim
+++ src/nimpathpkg/parser.nim
@@ -1,4 +1,5 @@
-import system, strutils, termios, posix, bitops, sequtils, tables, strformat, options, futhark
+import system, strutils, sequtils, tables, strformat, options, futhark
+export options
 
 const clangResourceDir {.strdefine.}: string = "nothing" # defined by config.nims
 importc:
@@ -18,14 +19,14 @@ proc cstringToNim(cst : cstring) : Option[string] =
   none(string)
 
 type HTMLNode = ref object of RootObj
-  node*: xmlNode
+  node*: ptr xmlNode
   context*: xmlXPathContextPtr
 
 proc textContent*(node: HTMLNode): Option[string] =
-  return node.node.addr.xmlNodeGetContent.cstringToNim
+  return node.node.xmlNodeGetContent.cstringToNim
 
 proc `$` (node: HTMLNode): string =
-  "node"
+  $node.node[]
 
 iterator query*(xpath_expr: string, xpath_ctx : xmlXPathContextPtr): HTMLNode =
   var xpath_obj : xmlXPathObjectPtr = xmlXPathEvalExpression(cast[ptr xmlChar](xpath_expr.cstring), xpath_ctx);
@@ -35,21 +36,20 @@ iterator query*(xpath_expr: string, xpath_ctx : xmlXPathContextPtr): HTMLNode =
 
   for i in (0..(nodes.nodeNr-1)):
     currentNode = cast[ptr xmlNodePtr](cast[int](nodes.nodeTab) + cast[int](i * nodes.nodeTab.sizeof))
-    #echo currentNode[].name, ", ", currentNode[].xmlNodeGetContent
-    yield HTMLNode(node: currentNode[][], context: xpath_ctx)
+    yield HTMLNode(node: currentNode[], context: xpath_ctx)
 
 iterator queryWithContext*(node: HTMLNode, xpath_expr: string) : HTMLNode =
   var oldContext = node.context[]
-  discard xmlXPathSetContextNode(node.node.addr, node.context)
+  discard xmlXPathSetContextNode(node.node, node.context)
   for node in query(xpath_expr, node.context):
     yield node
 
   node.context = oldContext.addr
 
-iterator parseTree*(input: string, xpath_expr: string) : HTMLNode =
+iterator parseTree*(input: string, xpath_expr: string, base_url: string) : HTMLNode =
   var input_p : cstring = input.cstring
   var input_p_size : cint = input_p.len.cint
-  var base_url : cstring = "https://example.org".cstring # TODO pass as parameter
+  var base_url : cstring = base_url.cstring
   var encoding : cstring = nil # TODO, different encodings?
   var options : cint = (XML_PARSE_RECOVER.int or XML_PARSE_NOWARNING.int or XML_PARSE_HUGE.int or XML_PARSE_NONET.int).cint
 
@@ -60,15 +60,11 @@ iterator parseTree*(input: string, xpath_expr: string) : HTMLNode =
   if (xpath_ctx == nil):
     xpath_ctx.xmlXPathFreeContext
     parser_result.xmlFreeDoc
-    #return # TODO raise exceptions
-    #
+
   for node in query(xpath_expr, xpath_ctx):
     yield node
 
   xpath_ctx.xmlXPathFreeContext
   parser_result.xmlFreeDoc
 
-  #xmlInitParser()
-
-  #xmlCleanupParser()
-  #xmlMemoryDump()
+  xmlCleanupParser()
