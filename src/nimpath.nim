b2d08e47a10d3043134228ca25ec1b38bb362362
Wesley Kerfoot <wjak56@gmail.com>
2022-12-21 15:27:50 -0500

    update main module

--- src/nimpath.nim
+++ src/nimpath.nim
@@ -1,6 +1,10 @@
 import nimpathpkg/parser
+export parser
 
 when isMainModule:
-  for node in parseTree("<html><body><h1>foobar</h1><h2>sdasdasd</h2><h1><p>this is a p</p><p>this is another p</p></h1></body></html>", "//*[self::h1 or self::h2]"):
-    for subnode in queryWithContext(node, "p"):
-      echo subnode.textContent
+  var testFile = "./test.html".open(fmRead)
+  for node in parseTree(testFile.readAll, "//*[self::h1 or self::div]"):
+    for subnode in queryWithContext(node, "span"):
+      echo subnode.textContent.get
+    if node.node.name == "h1":
+      echo node.textContent.get
